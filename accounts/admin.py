4bf139ed12224e1ecaf090c0e2f13bc0d552d845
Meraj Bighamian <merajbighamian@gmail.com>
2023-10-07 11:13:39 +0330

    chore: hash user password in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,8 +5,20 @@ from .models import CustomUser,Roles
 
 
 class CustomUserAdmin(UserAdmin):
-    list_display=['email']
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
     ordering=('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number', 'id_number', 'code')}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'roles', 'employer', 'emails')}),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
 
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
 admin.site.register(CustomUser,CustomUserAdmin)
 admin.site.register(Roles)
