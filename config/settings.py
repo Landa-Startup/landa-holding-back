9ce62927dd040b793eaa453e49aa50ced9b97d2d
Meraj Bighamian <merajbighamian@gmail.com>
2023-10-29 15:15:44 +0330

    chore: add email template for send email and convert gregorian to jalali

--- config/settings.py
+++ config/settings.py
@@ -220,6 +220,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
   # It will work instead of the default serializer(TokenObtainPairSerializer).
   "TOKEN_OBTAIN_SERIALIZER": "accounts.serializers.MyTokenObtainPairSerializer",
+  "ACCESS_TOKEN_LIFETIME": datetime.timedelta(minutes=120),
   # ...
 }
 
