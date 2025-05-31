3c201928ac98f7f33da571c0fb76065d086d53bf
amir-esfahani <ad.esfahanizadeh@yahoo.com>
2025-05-31 11:49:15 +0330

    fix: email function added to all of the forms

--- forms/serializers.py
+++ forms/serializers.py
@@ -2,28 +2,84 @@ from .models import StartUpsForm,ContactUs,PartnerMembership,InvestorRegistratio
 from rest_framework import serializers
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+import logging
+
+logger = logging.getLogger(__name__)
 
 class StartupFormSerializer(serializers.ModelSerializer):
     financialModelFile = serializers.FileField(required=False)
     financialFile = serializers.FileField(required=False)
     pitchDeckFile = serializers.FileField(required=False)
     businessPlanFile = serializers.FileField(required=False)
+
     class Meta:
         model = StartUpsForm
         fields = '__all__'
-        read_only_fields = ['id','createdAt']
+        read_only_fields = ['id', 'createdAt']
+
+    def create(self, validated_data):
+        instance = super().create(validated_data)
+
+        subject = 'Thank you for registering your startup'
+        from_email = 'amir.esfahanizadeh@landaholding.com'  # Use the same email as in EMAIL_HOST_USER
+        to_email = instance.email
+        context = {'first_name': instance.firstName}
+        text_content = f"Hi {instance.firstName},\n\nThanks for registering your startup with us."
+        html_content = render_to_string('startup_registration_email.html', context)
+
+        email = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
+        email.attach_alternative(html_content, "text/html")
+
+        try:
+            email.send()
+        except Exception as e:
+            logger.error(f"Failed to send registration email: {e}")
+
+        return instance
     
 class ContactUsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactUs
         fields = '__all__'
-        read_only_fields = ['id','createdAt']
+        read_only_fields = ['id', 'createdAt']
+
+    def create(self, validated_data):
+        instance = super().create(validated_data)
+
+        subject = 'Thanks for contacting us!'
+        from_email = 'amir.esfahanizadeh@landaholding.com'
+        to_email = instance.email
+        context = {'name': instance.name}
+        text_content = f"Hi {instance.name},\n\nThanks for reaching out. We'll respond to your message shortly."
+        html_content = render_to_string('contact_us_email.html', context)
+
+        email = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
+        email.attach_alternative(html_content, "text/html")
+        email.send()
+
+        return instance
 
 class PartnerMembershipSerializer(serializers.ModelSerializer):
     class Meta:
         model = PartnerMembership
         fields = '__all__'
-        read_only_fields = ['id','createdAt']
+        read_only_fields = ['id', 'createdAt']
+
+    def create(self, validated_data):
+        instance = super().create(validated_data)
+
+        subject = 'Thanks for joining our partner network'
+        from_email = 'amir.esfahanizadeh@landaholding.com'
+        to_email = instance.email
+        context = {'name': instance.name}
+        text_content = f"Hi {instance.name},\n\nWe're excited to have you as a partner!"
+        html_content = render_to_string('partner_membership_email.html', context)
+
+        email = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
+        email.attach_alternative(html_content, "text/html")
+        email.send()
+
+        return instance
     
 class InvestorRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
