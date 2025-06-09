3e47ee1c850a0825f337258361462daccce50854
amir-esfahani <ad.esfahanizadeh@yahoo.com>
2025-06-09 10:37:47 +0330

    minor fix

--- forms/urls.py
+++ forms/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import StartUpsFormView,ContactUsView,PartnerMembershipView,InvestorRegistrationView,CSRFTokenView
+from .views import StartUpsFormView,ContactUsView,PartnerMembershipView,InvestorRegistrationView,CSRFTokenView, EntrepreneurView
 
 
 app_name = "forms"
@@ -8,6 +8,6 @@ urlpatterns = [
     path('contactUs-form',ContactUsView.as_view(),name='contactus-form'),
     path('partner-membership',PartnerMembershipView.as_view(),name='partner-membership'),
     path('investor-registration',InvestorRegistrationView.as_view(),name='investor-registration'),
-    path('entrepreuneur-form',InvestorRegistrationView.as_view(),name='entrepreuneur-form'),
+    path('entrepreuneur-form',EntrepreneurView.as_view(),name='entrepreuneur-form'),
     path('get-csrf-token', CSRFTokenView.as_view(), name='get_csrf_token'),
 ]
